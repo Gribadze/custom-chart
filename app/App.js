b3a8ddf28e80ec839e9069b1019157ffbe78d603
Dmitry Fiodorov <d.fiodorov@vironit.co.uk>
2019-04-17 18:44:15 +0300

    line groups implemented

--- app/App.js
+++ app/App.js
@@ -9,19 +9,20 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import BarChart from './Components/BarChart';
+import LineChart from './Components/LineChart';
 
 const data = {
   January: { '2018': -20, '2019': -25 },
   February: { '2018': -10, '2019': -15 },
   March: { '2018': 3, '2019': -5 },
   April: { '2018': 10, '2019': 5 },
-  May: { '2018': -20, '2019': -25 },
-  June: { '2018': -20, '2019': -25 },
-  July: { '2018': -20, '2019': -25 },
-  August: { '2018': -20, '2019': -25 },
-  September: { '2018': -20, '2019': -25 },
-  October: { '2018': -20, '2019': -25 },
-  November: { '2018': -20, '2019': -25 },
+  May: { '2018': 15, '2019': 20 },
+  June: { '2018': 20, '2019': 25 },
+  July: { '2018': 25, '2019': 25 },
+  August: { '2018': 20, '2019': 30 },
+  September: { '2018': 15, '2019': 25 },
+  October: { '2018': 10, '2019': 5 },
+  November: { '2018': -10, '2019': -5 },
   December: { '2018': -20, '2019': -25 },
 };
 
@@ -35,6 +36,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <BarChart data={data} coloring={['yellow', 'blue']} labelRotation={60} vertical />
+      <LineChart data={data} coloring={['yellow', 'blue']} />
     </View>
   );
 }
