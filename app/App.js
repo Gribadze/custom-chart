614db1480f6143adcf5294d0d863be98a83f3994
Dmitry Fiodorov <d.fiodorov@vironit.co.uk>
2019-04-17 20:24:11 +0300

    designed pie

--- app/App.js
+++ app/App.js
@@ -8,6 +8,7 @@
 
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Circle, Line, Svg } from 'react-native-svg';
 import BarChart from './Components/BarChart';
 import LineChart from './Components/LineChart';
 
@@ -33,10 +34,44 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const d = {
+    Male: 0.33,
+    Female: 0.45,
+    Bot: 0.22,
+  };
+  const r = 150;
+  const len = 2 * Math.PI * (r / 2);
   return (
     <View style={styles.container}>
       <BarChart data={data} coloring={['yellow', 'blue']} labelRotation={60} vertical />
       <LineChart data={data} coloring={['green', 'cyan']} />
+      <Svg width="100%" height="100%" viewBox="-200 -200 500 500">
+        <Circle
+          r={r / 2}
+          fill="none"
+          stroke="cyan"
+          strokeDasharray={`${len * d.Male} ${len}`}
+          strokeWidth={r}
+        />
+        <Circle
+          r={r / 2}
+          fill="none"
+          stroke="magenta"
+          strokeDashoffset={`${-len * d.Male}`}
+          strokeDasharray={`${len * d.Female} ${len}`}
+          strokeWidth={r}
+        />
+        <Circle
+          r={r / 2}
+          fill="none"
+          stroke="gray"
+          strokeDashoffset={`${-len * (d.Male + d.Female)}`}
+          strokeDasharray={`${len * d.Bot} ${len}`}
+          strokeWidth={r}
+        />
+        <Line x1={-200} y1={0} x2={300} y2={0} stroke="silver" />
+        <Line x1={0} y1={-200} x2={0} y2={300} stroke="silver" />
+      </Svg>
     </View>
   );
 }
